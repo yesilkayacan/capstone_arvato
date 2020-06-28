8ac81f068426c35c3ddfee46c4a4d30f4c1d5103
CY <yesilkayacan@gmail.com>
2020-06-28 19:47:38 +0300

    refactor: impute_na moved back to etl.py

--- fresh/etl/etl.py
+++ fresh/etl/etl.py
@@ -1,7 +1,7 @@
 import numpy as np
 import progressbar
 import pandas as pd
-
+import copy
 
 class Data_Correction():
     
@@ -15,7 +15,10 @@ class Data_Correction():
 
         '''
         
-        check_dict = mergeDict(self.mapping.known_mapping, self.mapping.unknown_mapping)
+        known_mapping = copy.deepcopy(self.mapping.known_mapping)
+        unknown_mapping = copy.deepcopy(self.mapping.unknown_mapping)
+
+        check_dict = mergeDict(known_mapping, unknown_mapping)
         [check_dict[x].append(np.nan) for x in check_dict.keys()]
         
         all_keys = check_dict.keys()
@@ -302,8 +305,52 @@ def etl_transform(df, attr_mapping, ref_cols=None):
         print('getting the subset of the data with the reference features...')
         df_clean = df_clean[ref_cols]
     
-    
+    print('Imputing missing values...')
+    df_clean = impute_na(df_clean)
+
     print('Ratio of data used')
     print('features: %.2f' % (df_clean.shape[1]/df.shape[1]*100))
 
     return df_clean
+
+
+def impute_na(df):
+    '''Impute data inplace of missing values. Uses median for quantitative 
+    data and most frequent for qualitative data.'   
+    
+    ARGS
+    ----
+    df: (pandas.DataFrame) Dataframe where the missing values will be replaced
+
+    RETURNS
+    -------
+    df_impute: (pandas.DataFrame) Copy of df where the missing values have been imputed
+    '''
+    
+    df_impute = df.copy()
+    
+    qualitative_features_used, numeric_features_used = AttributeMapping.get_feature_types(df_impute)
+    
+    print('Imputing quantitative features...')
+    cnter = 0
+    bar = progressbar.ProgressBar(maxval=len(numeric_features_used)+1, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+    bar.start()
+    # impute median for missing values in quantitative features
+    for feat in numeric_features_used:
+        df_impute[feat] = df_impute[feat].fillna(df_impute[feat].median())
+        cnter+=1 
+        bar.update(cnter)
+    bar.finish()
+    
+    print('Imputing qualitative features...')
+    cnter = 0
+    bar = progressbar.ProgressBar(maxval=len(qualitative_features_used)+1, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+    bar.start()
+    # impute mode (most frequent) for missing values in qualitative features
+    for feat in qualitative_features_used:
+        df_impute[feat] = df_impute[feat].fillna(df_impute[feat].mode().iloc[0])
+        cnter+=1 
+        bar.update(cnter)
+    bar.finish()
+    
+    return df_impute
